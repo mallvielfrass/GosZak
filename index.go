f4b71e39d3254cc564ed14d0767ee890353da028
Rason <Dummy>
2019-04-15 21:10:54 +0500

    HTML edits

--- index.go
+++ index.go
@@ -7,3 +7,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "main.html")
 
 }
+func about(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "about.html")
+}
